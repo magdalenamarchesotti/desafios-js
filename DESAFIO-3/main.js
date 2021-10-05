005677230f101a37e025c4b5c2dcd4492c1c9b53
Magdalena Marchesotti <magdalenamarchesotti@gmail.com>
2021-10-05 12:54:11 +0100

    desafio 3

---
+++ DESAFIO-3/main.js
@@ -0,0 +1,6 @@
+let ingresarNumero = parseInt(prompt("Ingresá un número"));
+for (let i = 2; i <= 10; i+=2) {
+    let resultado = ingresarNumero * i;
+    alert(ingresarNumero + " x " + i + " = " + resultado);
+}
+
